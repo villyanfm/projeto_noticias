14db0c3abe9cb852b8fa77fff11bf1114d4f0e99
Villyan <150153700+villyanfm@users.noreply.github.com>
2025-12-09 16:29:47 -0400

    Update [tiponoticia].js

--- src/pages/noticia/tipo/[tiponoticia].js
+++ src/pages/noticia/tipo/[tiponoticia].js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "../../components/cards";
 export async function getServerSideProps(req) {
   // Fetch data from  API notícias
-  const res = await fetch('https://apivillyanb.vercel.app/noticias' + req.query.tiponoticia)
+  const res = await fetch('apivillyanb-rj9ehp4a1-villyans-projects.vercel.appnoticias' + req.query.tiponoticia)
   const repo = await res.json()
   // Pass data to the page via props ola
   return { props: { noticias: repo } }
