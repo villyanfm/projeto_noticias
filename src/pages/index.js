60992790f6b012f27de37a412c83071caa9e0b75
Villyan <150153700+villyanfm@users.noreply.github.com>
2025-12-11 11:59:05 +0000

    oi

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "./components/cards";
 export async function getServerSideProps() {
   // Fetch data from  API notícias
-  const res = await fetch('https://apivillyanb.vercel.app/noticias')
+  const res = await fetch('https://api-villyan-b.vercel.app/noticias')
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
